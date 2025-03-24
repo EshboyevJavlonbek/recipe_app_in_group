e9d0f8dc5b6223c486475b71a735bdebbd435216
EshboyevJavlonbek <javlonbekgaming18@gmail.com>
2025-03-24 15:18:43 +0500

    10

--- lib/features/top_chefs/pages/top_chefs_view.dart
+++ lib/features/top_chefs/pages/top_chefs_view.dart
@@ -35,7 +35,7 @@ class TopChefsView extends StatelessWidget {
         ),
         extendBody: true,
         body: Column(
-          spacing: 11.h,
+          spacing: 10.h,
           children: [
             MostViewedChefsSection(mostViewed: state.mostViewedChefs),
             Expanded(
